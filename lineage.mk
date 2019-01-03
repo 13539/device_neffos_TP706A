1993d822d564a19e6d3c6b3d9eb455b477335d9b
Andrew <poisk567@gmail.com>
2019-01-03 21:28:31 +0200

    first commit

---
+++ lineage.mk
@@ -0,0 +1,11 @@
+# Release name
+PRODUCT_RELEASE_NAME := C9a
+
+$(call inherit-product, device/neffos/C9a/device_C9a.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := C9a
+PRODUCT_NAME := lineage_C9a
+PRODUCT_BRAND := neffos
+PRODUCT_MODEL := C9a
+PRODUCT_MANUFACTURER := Tp-Link
